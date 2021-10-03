302b1b97796319587784244889e5bcab81919bef
bogdanzaec <zaietsbohdan504@gmail.com>
2021-10-03 11:31:22 -0400

    updating first file

--- src/day5/First.java
+++ src/day5/First.java
@@ -3,5 +3,10 @@ package day5;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        // this is a new line
+        // I want to create history
+        // text doesn't matter
+
     }
 }
