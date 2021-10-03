d8206ee0634417d50c5f3acb7033a393679978c6
bogdanzaec <zaietsbohdan504@gmail.com>
2021-10-03 16:19:50 -0400

    made car in Master branch 2

--- src/day5/Car.java
+++ src/day5/Car.java
@@ -1,17 +1,15 @@
 package day5;
 
 public class Car {
-<<<<<<< HEAD
 
     // this is car class from master branch
 
     // we will have conflict, because it is different text in different branches for car
-=======
+
     // this is our car from James branch
 
     // more text
 
     // more text
 
->>>>>>> James
 }
