d0d9d22fcb66acfef7c26497695081487f8776ee
bogdanzaec <zaietsbohdan504@gmail.com>
2021-10-03 12:23:41 -0400

    Updated second file again

--- src/day5/Second.java
+++ src/day5/Second.java
@@ -3,15 +3,15 @@ package day5;
 public class Second {
 
     public void m(){
-
+        System.out.println("first method");
     }
 
     public void m(int a){
-
+        System.out.println("second");
     }
 
     public void m(String s){
-
+        System.out.println("third");
 
     }
 }
